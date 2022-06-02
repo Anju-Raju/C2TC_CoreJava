f76a42f42d1b4ab12fee15047b95d9ee225d7c8f
AnnieMol R <AnnieMol R@DESKTOP-6J90AVM>
2022-06-02 15:53:02 +0530

    First commit

---
+++ CoreJava(CAP)/src/Projects/ArraySum.java
@@ -0,0 +1,17 @@
+package Projects;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[] = {10,20,30,40,50};
+		int sum=0;
+		for(int i=0;i<array.length;i++)
+		{
+			sum+=array[i];
+		}
+		System.out.println("Summ of Array Elements = "+sum);
+
+	}
+
+}
